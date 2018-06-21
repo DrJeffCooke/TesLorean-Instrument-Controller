48ae53379c6ead9537948d6be87536b0e625d726
Jeff Cooke <24613307+DrJeffCooke@users.noreply.github.com>
2018-06-21 14:04:35 -0500

    Add files via upload

---
+++ Real_Time_Clock_v1a.ino
@@ -0,0 +1,43 @@
+#include <Wire.h>
+#include <DS3231.h>
+
+// Code from the Demo Example of the DS3231 Library
+DS3231  rtc(SDA, SCL);
+
+void setup()
+{
+  // Setup Serial connection
+  Serial.begin(9600);
+  // Uncomment the next line if you are using an Arduino Leonardo
+  //while (!Serial) {}
+  
+  // Initialize the rtc object
+  rtc.begin();
+  
+  // The following lines can be uncommented to set the date and time
+  rtc.setDOW(SUNDAY);        // Set Day-of-Week to SUNDAY
+  rtc.setTime(9,0,0);     // Set the time to 12:00:00 (24hr format)
+  rtc.setDate(12, 17, 2017);   // Set the date to January 1st, 2014
+}
+
+// Code from the Demo Example of the DS3231 Library
+void loop()
+{
+  // Send Day-of-Week
+  Serial.print(rtc.getDOWStr());
+  Serial.print(" ");
+  
+  // Send date
+  Serial.print(rtc.getDateStr());
+  Serial.print(" -- ");
+
+  // Send time
+  Serial.println(rtc.getTimeStr());
+
+  // Get Temp
+  Serial.print("Temp :");
+  Serial.println(rtc.getTemp());
+  
+  // Wait one second before repeating
+  delay (1000);
+}
